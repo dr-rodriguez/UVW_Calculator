79273b24e22f12d3f76a69881a72c990bdb3d46e
David Rodriguez <strakul@gmail.com>
2015-08-01 14:14:39 -0300

    finish uvw_err.py and brief testing

--- uvw_errs.py
+++ uvw_errs.py
@@ -6,8 +6,9 @@ June 16, 2015
 Translation of my uvw_errs.pro routing from IDL
 """
 
+import sys
 from math import cos, sin
-#from pylab import double
+from pylab import mean, std #double
 from random import seed, gauss
 
 # ============================================
@@ -50,27 +51,27 @@ if len(sys.argv)<6:
 	"""
 	sys.exit()
 
-ra 		= sys.argv[1]
-dec 	= sys.argv[2]
-pmra0 	= sys.argv[3]
-pmdec0 	= sys.argv[4]
-dist0 	= sys.argv[5]
-rv0 	= sys.argv[6]
+ra 		= float(sys.argv[1])
+dec 	= float(sys.argv[2])
+pmra0 	= float(sys.argv[3])
+pmdec0 	= float(sys.argv[4])
+dist0 	= float(sys.argv[5])
+rv0 	= float(sys.argv[6])
 
 if len(sys.argv)>7:
-        pmra_err = sys.argv[7]
-        pmde_err = sys.argv[8]
+        pmra_err = float(sys.argv[7])
+        pmde_err = float(sys.argv[8])
 else:
         pmra_err = 0.0
         pmde_err = 0.0
 
 if len(sys.argv)>9:
-        rverr = sys.argv[9]
+        rverr = float(sys.argv[9])
 else:
         rverr = 0.0
 
 if len(sys.argv)>10:
-        derr = sys.argv[10]
+        derr = float(sys.argv[10])
 else:
         derr = 0.0
 
@@ -87,15 +88,41 @@ w = []
 
 # Compute UVW using the error terms
 for i in range(num):
-	if rverr>0: rv = gauss(rv0,rverr) else rv = rv0
-	if pmra_err>0: pmra = gauss(pmra0,pmra_err) else pmra = pmra0
-	if pmde_err>0: pmdec = gauss(pmdec0,pmde_err) else pmdec = pmdec0
-	if derr>0: dist = guass(dist0,derr) else dist = dist0
+	if rverr>0: 
+		rv = gauss(rv0,rverr) 
+	else: 
+		rv = rv0
 
-	u0,v0,w0 = uvw(ra,dec,dist,pmra,pmde,rv)
+	if pmra_err>0: 
+		pmra = gauss(pmra0,pmra_err) 
+	else: 
+		pmra = pmra0
+
+	if pmde_err>0: 
+		pmdec = gauss(pmdec0,pmde_err) 
+	else: 
+		pmdec = pmdec0
+
+	if derr>0: 
+		dist = gauss(dist0,derr) 
+	else: 
+		dist = dist0
+
+	u0,v0,w0 = uvw(ra,dec,dist,pmra,pmdec,rv)
 	u.append(u0)
 	v.append(v0)
 	w.append(w0)
 
 # Compute mean and standard deviation for UVW
+u_val = mean(u)
+v_val = mean(v)
+w_val = mean(w)
+u_err = std(u)
+v_err = std(v)
+w_err = std(w)
+
+print
+print 'U: %3.2f +/- %1.2f' % (u_val, u_err)
+print 'V: %3.2f +/- %1.2f' % (v_val, v_err)
+print 'W: %3.2f +/- %1.2f' % (w_val, w_err)
 
